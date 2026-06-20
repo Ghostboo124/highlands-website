66c6ce292ccef90749127b5cc9f3b62015e01f3e
Lexy <lexy@lexy.boo>
2026-06-21 01:13:01 +1000

    fix: fix errors in page.jsx
    Signed-off-by: Lexy <lexy@lexy.boo>

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex flex-col bg-gray-700 font-sans justify-center items-center">
-      <div className="text-center bg-yellow-500 border-2 hover:scale-110 transition-all border-black rounded-2xl w-[1000px] mt-16 justify-center">
+      <div className="text-center bg-yellow-500 border-2 hover:scale-110 transition-all border-black rounded-2xl w-250 mt-16 justify-center">
         <h1 className="text-black text-5xl">
           {" "}
           Welcome to Highlands YSWS!!! ⛰️
@@ -16,16 +16,17 @@ export default function Home() {
         <button
           url
           className="text-center bg-yellow-500 border-2 border-black font-bold text-2xl text-black h-16 hover:scale-125 cursor-pointer transition-all rounded-xl w-44 mt-16 justify-center"
+          type="button"
         >
           RSVP Now!
         </button>
       </Link>
-      <div className="text-center bg-green-400 border-2 border-black rounded-2xl w-[700px] content-center items-center mt-16 justify-center">
+      <div className="text-center bg-green-400 border-2 border-black rounded-2xl w-175 content-center items-center mt-16 justify-center">
         <h1 className="text-black text-5xl hover:scale-110 transition-all">
           {" "}
           What&#39;s this about?{" "}
         </h1>
-        <p className="text-black bg-blue-300 hover:scale-110 transition-all text-2xl w-[500px] text-center mt-4 border-2 border-black rounded-2xl justify-center items-center mx-auto">
+        <p className="text-black bg-blue-300 hover:scale-110 transition-all text-2xl w-125 text-center mt-4 border-2 border-black rounded-2xl justify-center items-center mx-auto">
           This is a mountain themed YSWS where you code hours and can earn
           prizes like earbuds, credits, games, 3d printers, and more! Plus
           we&#39;ll have hiking related stuff as prizes! And if you code for
@@ -33,12 +34,12 @@ export default function Home() {
           Armenia!
         </p>
       </div>
-      <div className="text-center bg-green-400 border-2 border-black rounded-2xl w-[700px] content-center items-center mt-16 justify-center">
+      <div className="text-center bg-green-400 border-2 border-black rounded-2xl w-175 content-center items-center mt-16 justify-center">
         <h1 className="text-black text-5xl hover:scale-110 transition-all">
           {" "}
           Where is Armenia?{" "}
         </h1>
-        <p className="text-black bg-blue-300 hover:scale-110 transition-all text-2xl w-[500px] text-center mt-4 border-2 border-black rounded-2xl justify-center items-center mx-auto">
+        <p className="text-black bg-blue-300 hover:scale-110 transition-all text-2xl w-125 text-center mt-4 border-2 border-black rounded-2xl justify-center items-center mx-auto">
           Armenia is in the Southern Caucasus, between Iran, Georgia, and
           Turkey! Armenia is considered the 10th most mountainous nation, hence
           the name &#34;Highlands.&#34; I would like this YSWS to happen in
@@ -47,12 +48,12 @@ export default function Home() {
           it&#39;s visa-free for most nations.
         </p>
       </div>
-      <div className="text-center bg-green-400 border-2 border-black rounded-2xl w-[700px] content-center items-center mt-16 justify-center">
+      <div className="text-center bg-green-400 border-2 border-black rounded-2xl w-175 content-center items-center mt-16 justify-center">
         <h1 className="text-black text-5xl hover:scale-110 transition-all">
           {" "}
           Is there anything in Yerevan?{" "}
         </h1>
-        <p className="text-black bg-blue-300 text-2xl hover:scale-110 transition-all w-[500px] text-center mt-4 border-2 border-black rounded-2xl justify-center items-center mx-auto">
+        <p className="text-black bg-blue-300 text-2xl hover:scale-110 transition-all w-125 text-center mt-4 border-2 border-black rounded-2xl justify-center items-center mx-auto">
           There are a lot of places to visit in Yerevan. Most famous sites in
           Yerevan include the cascade complex, Tsitsernakaberd, Republic Square,
           The National History Museum, the Opera, and even the Metro here + more
